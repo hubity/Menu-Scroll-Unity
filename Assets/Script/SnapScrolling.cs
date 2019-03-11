1ec093ba74a3cc0793b16108c92fcd2409fb216f
Danyllo Fellip <hubitygames@gmail.com>
2019-03-11 16:06:27 -0300

    adding arrow

--- Assets/Script/SnapScrolling.cs
+++ Assets/Script/SnapScrolling.cs
@@ -1,60 +1,106 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SnapScrolling : MonoBehaviour
 {
     [Range(1, 50)]
-    [Header("Controller")]
+    [Header("Controllers")]
     public int panCount;
     [Range(0, 500)]
     public int panOffset;
+    [Range(0f, 20f)]
+    public float snapSpeed;
+    [Range(0f, 10f)]
+    public float scaleOffset;
+    [Range(1f, 20f)]
+    public float scaleSpeed;
     [Header("Other Objects")]
     public GameObject panPrefab;
+    public ScrollRect scrollRect;
 
-    private GameObject[] insPans; 
+    private GameObject[] instPans;
     private Vector2[] pansPos;
-     
+    private Vector2[] pansScale;
+
     private RectTransform contentRect;
+    private Vector2 contentVector;
 
     public int selectedPanID;
-    public bool isScrolling;
-    // Start is called before the first frame update
-    void Start()
-    {
-        contentRect = GetComponent<RectTransform>();
-        insPans = new GameObject[panCount];
-        pansPos = new Vector2[panCount];
-        for(int i = 0; i < panCount; i++)
-        {
-            insPans[i] = Instantiate(panPrefab, transform, false);
-            if (i == 0) continue;
-            {
-                insPans[i].transform.localPosition = new Vector2(insPans[i-1].transform.localPosition.x + panPrefab.GetComponent<RectTransform>().sizeDelta.x + panOffset,
-                    insPans[i].transform.localPosition.y);
-                pansPos[i] = insPans[i].transform.localPosition;
-            }
-        }
-        
-    }
+    private bool isScrolling;
 
-    // Update is called once per frame
-    void FixedUpdate()
+    public int j;
+
+    private void Start()
     {
+
+        contentRect = GetComponent<RectTransform>();
+        instPans = new GameObject[panCount];
+        pansPos = new Vector2[panCount];
+        pansScale = new Vector2[panCount];
+        for (int i = 0; i < panCount; i++)
+        {
+            instPans[i] = Instantiate(panPrefab, transform, false);
+            if (i == 0) continue;
+            instPans[i].transform.localPosition = new Vector2(instPans[i - 1].transform.localPosition.x + panPrefab.GetComponent<RectTransform>().sizeDelta.x + panOffset,
+                instPans[i].transform.localPosition.y);
+            pansPos[i] = -instPans[i].transform.localPosition;
+        }
+    }
+    public int local;
+    private void FixedUpdate()
+    {
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            local = local + 1;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            local = local - 1;
+        }
+
+        if (contentRect.anchoredPosition.x >= pansPos[0].x && !isScrolling || contentRect.anchoredPosition.x <= pansPos[pansPos.Length - 1].x && !isScrolling)
+            scrollRect.inertia = false;
         float nearestPos = float.MaxValue;
         for (int i = 0; i < panCount; i++)
         {
-            float distance = Mathf.Abs(contentRect.anchoredPosition.x + pansPos[i].x);
-            if(distance < nearestPos)
+            
+            float distance = Mathf.Abs(contentRect.anchoredPosition.x - pansPos[i].x);
+            if (distance < nearestPos)
             {
+
                 nearestPos = distance;
-                selectedPanID = i;
+                selectedPanID = local;
             }
+            float scale = Mathf.Clamp(1 / (distance / panOffset) * scaleOffset, 0.5f, 1f);
+            pansScale[i].x = Mathf.SmoothStep(instPans[i].transform.localScale.x, scale + 0.3f, scaleSpeed * Time.fixedDeltaTime);
+            pansScale[i].y = Mathf.SmoothStep(instPans[i].transform.localScale.y, scale + 0.3f, scaleSpeed * Time.fixedDeltaTime);
+            instPans[i].transform.localScale = pansScale[i];
+            
         }
+        float scrollVelocity = Mathf.Abs(scrollRect.velocity.x);
+        if (scrollVelocity < 400 && !isScrolling) scrollRect.inertia = false;
+        if (isScrolling || scrollVelocity > 400) return;
+        contentVector.x = Mathf.SmoothStep(contentRect.anchoredPosition.x, pansPos[selectedPanID].x, snapSpeed * Time.fixedDeltaTime);
+        contentRect.anchoredPosition = contentVector;
     }
 
     public void Scrolling(bool scroll)
     {
         isScrolling = scroll;
+        if (scroll) scrollRect.inertia = true;
     }
+
+    public void Teste()
+    {
+        //float nearestPos = float.MaxValue;
+        for (int i = 0;i < panCount; i++)
+        {
+            
+            
+        }
+    }
+
 }
